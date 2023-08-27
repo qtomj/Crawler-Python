e47d9252beaa6507326454b4dacff2b8f6a5d0d6
qtomj <137913908+qtomj@users.noreply.github.com>
2023-08-27 17:36:48 +0800

    Create workflow_download_image.py

---
+++ usage/workflow_download_image.py
@@ -0,0 +1,35 @@
+from jmcomic import create_option, JmcomicClient, JmcomicText
+
+opfile = '../assets/config/option_workflow_download.yml'
+
+option = create_option(opfile)
+client: JmcomicClient = option.build_jm_client()
+
+def get_jm_album_ids():
+    from common import str_to_set
+
+    jm_albums = []
+    aid_set = set()
+    for text in [
+        jm_albums,
+        (get_env('JM_ALBUM_IDS') or '').replace('-', '\n'),
+    ]:
+        aid_set.update(str_to_set(text))
+
+    return aid_set
+
+def download_by_url(album_id, url: str):
+    photo_id = JmcomicText.parse_to_photo_id(url)  # 400222
+    filename = url[url.rindex('/'):url.rindex('?')]  # 00001.webp
+    default_save_dir = get_env('JM_DOWNLOAD_DIR')
+    client.download_image(
+        img_url=url,
+        img_save_path=f'{default_dir}/{album_id}/{filename}',
+    )
+
+
+if __name__ == '__main__':
+    for album_id in get_jm_album_ids():
+        urls = ['https://cdn-msp.jm-comic1.club/media/photos/{0}/{1}.webp?v=1692375790'.format(album_id, str(i).zfill(5)) for i in range(150)]
+        for url in urls:
+            download_by_url(album_id, url)
